366287d6c9a1be0041401ca30d536462f38889a4
Roukmoute <contact@roukmoute.fr>
2017-01-31 00:34:45 +0100

    Prepare for more complexity with template method

--- src/TemplateMethod/index.php
+++ src/TemplateMethod/index.php
@@ -6,8 +6,20 @@ use KnpU\DesignPatterns\TemplateMethod\SaurischiaType;
 
 require dirname(__FILE__, 3) . '/vendor/autoload.php';
 
-$saurischia = new SaurischiaType();
-echo $saurischia->getInformations();
+if (!array_key_exists(1, $argv)) {
+    throw new \RuntimeException('Type of dinosaur missing.');
+}
 
-$ornithischiaType = new OrnithischiaType();
-echo $ornithischiaType->getInformations();
+$type = mb_strtolower($argv[1]);
+
+if (!in_array($type, ['saurischia', 'ornithischia'])) {
+    throw new \Exception(sprintf('Your type of dinosaur : "%s" is incorrect', $type));
+}
+
+if ($type === 'saurischia') {
+    $dinosaurType = new SaurischiaType();
+} elseif ($type === 'ornithischia') {
+    $dinosaurType = new OrnithischiaType();
+}
+
+echo $dinosaurType->getInformations();
